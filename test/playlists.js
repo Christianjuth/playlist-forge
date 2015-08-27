d5fa59d73ed31a312bfb62961757f569018ec53f
Christian Juth <cjuth2@gmail.com>
2015-08-27 10:21:30 -0400

    test update

--- test/playlists.js
+++ test/playlists.js
@@ -1,15 +1,15 @@
-const Browser = require('zombie');
+var Browser = require('zombie');
 
-// We're going to make requests to http://example.com/signup
-// Which will be routed to our test server localhost:3000
+// We're going to make requests to localhost:3000
 Browser.localhost('0.0.0.0', 3000);
+var browser = new Browser();
 
+
+// All your tests live inside this block
 describe('User visits playlist page', function() {
 
-  const browser = new Browser();
-
   before(function(done) {
-    return browser.visit('/all-playlists', done);
+    browser.visit('/all-playlists', done);
   });
 
   describe('Check title', function() {
