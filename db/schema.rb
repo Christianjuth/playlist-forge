566c0d00745edb8c622ea9458ab3eb6dac4fe053
Christian Juth <cjuth2@gmail.com>
2015-08-27 10:35:53 -0400

    fixed table

--- db/schema.rb
+++ db/schema.rb
@@ -14,9 +14,9 @@
 ActiveRecord::Schema.define(version: 20150825212914) do
 
   create_table "users", force: :cascade do |t|
-    t.string  "username"
-    t.string  "email"
-    t.integer "spotify_uid"
+    t.string "username"
+    t.string "email"
+    t.string "spotify_uid"
   end
 
 end
